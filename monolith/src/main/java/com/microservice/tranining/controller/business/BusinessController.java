fbfee40e65d49393a872b46d4a6fd4c041860ad8
pavlo.mykhaylenko <pavlo.mykhaylenko@globallogic.com>
2022-09-07 15:10:21 +0300

    Add basic circuit breaker
    Add Library circuit breaker. Will call BKP method if basic method fails.
    Add new Business Controller for Testing purposes.

---
+++ monolith/src/main/java/com/microservice/tranining/controller/business/BusinessController.java
@@ -0,0 +1,59 @@
+package com.microservice.tranining.controller.business;
+
+import com.microservice.tranining.dto.Order;
+import com.microservice.tranining.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/business")
+public class BusinessController {
+
+        private static final String ORDER_SERVICE_URL = "http://orderms-service:8082/orders/";
+//    private static final String ORDER_SERVICE_URL = "http://localhost:8082/orders/";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
+    @GetMapping
+    public Iterable<Order> getAllOrders() {
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+
+        return circuitBreaker.run(() -> restTemplate.exchange(ORDER_SERVICE_URL,
+                                        HttpMethod.GET,
+                                        null,
+                                        new ParameterizedTypeReference<Iterable<Order>>() {
+                                        })
+                                        .getBody(),
+                                  throwable -> getAllOrdersBkp());
+    }
+
+
+    public Iterable<Order> getAllOrdersBkp() {
+        List<Order> allOrders = orderService.getAllOrders();
+        getBkpOrder(allOrders);
+        return allOrders;
+    }
+
+    private void getBkpOrder(List<Order> allOrders) {
+        Order bkpOrder = new Order();
+        bkpOrder.setId(999L);
+        bkpOrder.setStatus("BKP_ORDER_STATUS");
+        allOrders.add(bkpOrder);
+    }
+}
